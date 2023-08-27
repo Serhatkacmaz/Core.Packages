1d34b7fc8b9e1e6d7d844f0fa1d07fda12c99631
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-08-27 21:45:20 +0300

    recent changes

---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ExceptionMiddelwareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ExceptionMiddelwareExtensions
+{
+    public static void ConfigureCustomExceptionMiddelware(this IApplicationBuilder app)
+        => app.UseMiddleware<ExceptionMiddelware>();
+}
--- Core.Persistence/Dynamic/Filter.cs
+++ Core.Persistence/Dynamic/Filter.cs
@@ -9,8 +9,8 @@ namespace Core.Persistence.Dynamic;
 public class Filter
 {
     public string Field { get; set; }
-    public string Operator { get; set; }
     public string? Value { get; set; }
+    public string Operator { get; set; }
     public string? Logic { get; set; }
     public IEnumerable<Filter>? Filters { get; set; }
 
