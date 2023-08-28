26ededa6645ec0cae5d458c6d0beaddde02dcf08
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-08-28 18:23:15 +0300

    recent changes

--- Core.CrossCuttingConcerns/Exceptions/ExceptionMiddelware.cs
+++ Core.CrossCuttingConcerns/Exceptions/ExceptionMiddelware.cs
@@ -1,10 +1,14 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Core.CrossCuttingConcerns.Exceptions;
 
@@ -12,11 +16,15 @@ public class ExceptionMiddelware
 {
     private readonly RequestDelegate _next;
     private readonly HttpExceptionHandler _httpExceptionHandler;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly LoggerServiceBase _loggerServiceBase;
 
-    public ExceptionMiddelware(RequestDelegate next)
+    public ExceptionMiddelware(RequestDelegate next, IHttpContextAccessor httpContextAccessor, LoggerServiceBase loggerServiceBase)
     {
         _next = next;
         _httpExceptionHandler = new HttpExceptionHandler();
+        _httpContextAccessor = httpContextAccessor;
+        _loggerServiceBase = loggerServiceBase;
     }
 
     public async Task Invoke(HttpContext context)
@@ -27,8 +35,29 @@ public class ExceptionMiddelware
         }
         catch (Exception exception)
         {
+            await LogException(context, exception);
             await HandleExceptionAsync(context.Response, exception);
         }
+    }
+
+    private Task LogException(HttpContext context, Exception exception)
+    {
+        List<LogParameter> logParameters = new()
+        {
+            new LogParameter{ Type = context.GetType().Name, Value = exception.ToString()},
+        };
+
+        LogDetailWithException logDetailWithException = new()
+        {
+            ExceptionMessage = exception.Message,
+            MethodName = _next.Method.Name,
+            Parameters = logParameters,
+            User = _httpContextAccessor.HttpContext?.User.Identity?.Name ?? "?"
+        };
+
+        _loggerServiceBase.Error(JsonSerializer.Serialize(logDetailWithException));
+
+        return Task.CompletedTask;
     }
 
     private Task HandleExceptionAsync(HttpResponse response, Exception exception)
---
+++ Core.CrossCuttingConcerns/Serilog/Loggers/MsSqlLogger.cs
@@ -0,0 +1,34 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Loggers;
+
+public class MsSqlLogger : LoggerServiceBase
+{
+    public MsSqlLogger(IConfiguration configuration)
+    {
+        MsSqlConfiguration logConfig =
+             configuration.GetSection("SeriLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>()
+             ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        MSSqlServerSinkOptions sinkOptions = new()
+        {
+            TableName = logConfig.TableName,
+            AutoCreateSqlTable = logConfig.AutoCreateSqlTable,
+        };
+
+        ColumnOptions columnOptions = new();
+
+        Logger = new LoggerConfiguration().WriteTo
+             .MSSqlServer(logConfig.ConnectionString, sinkOptions, columnOptions: columnOptions)
+             .CreateLogger();
+    }
+}
